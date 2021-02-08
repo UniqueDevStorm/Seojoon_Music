92f9fb71768080c7beb6f45357db19a02ccb2dd0
unknown <storm@uniquecode.team>
2021-02-08 17:15:58 +0900

    refactor: black format

--- __main__.py
+++ __main__.py
@@ -4,16 +4,16 @@ import os
 from utils.autocogs import AutoCogs
 
 load_dotenv(verbose=True)
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv("TOKEN")
 
 
 class Bot(commands.AutoShardedBot):
     def __init__(self):
-        super().__init__(command_prefix='../')
+        super().__init__(command_prefix="../")
         AutoCogs(self)
 
     async def on_ready(self):
-        print(f'{str(self.user)} on Ready.')
+        print(f"{str(self.user)} on Ready.")
 
 
 bot = Bot()
--- cogs/Music.py
+++ cogs/Music.py
@@ -10,42 +10,57 @@ import os
 import time
 
 load_dotenv(verbose=True)
-BOTID = int(os.getenv('BOTID'))
-url_rx = re.compile(r'https?://(?:www\.)?.+')
+BOTID = int(os.getenv("BOTID"))
+url_rx = re.compile(r"https?://(?:www\.)?.+")
+
 
 class Music(commands.Cog):
     def __init__(self, bot: AutoShardedBot):
         self.bot = bot
-        if not hasattr(bot, 'lavalink'):  # This ensures the client isn't overwritten during cog reloads.
+        if not hasattr(
+            bot, "lavalink"
+        ):  # This ensures the client isn't overwritten during cog reloads.
             bot.lavalink = lavalink.Client(BOTID)
-            bot.lavalink.add_node('127.0.0.1', 2333, 'youshallnotpass', 'eu')  # Host, Port, Password, Region, Name
-            bot.add_listener(bot.lavalink.voice_update_handler, 'on_socket_response')
+            bot.lavalink.add_node(
+                "127.0.0.1", 2333, "youshallnotpass", "eu"
+            )  # Host, Port, Password, Region, Name
+            bot.add_listener(bot.lavalink.voice_update_handler, "on_socket_response")
         bot.lavalink.add_event_hook(self.track_hook)
 
     def cog_unload(self):
         self.bot.lavalink._event_hooks.clear()
 
     async def ensure_voice(self, ctx: Context):
-        player = self.bot.lavalink.player_manager.create(ctx.guild.id, endpoint=str(ctx.guild.region))
-        should_connect = ctx.command.name in ('play',)
+        player = self.bot.lavalink.player_manager.create(
+            ctx.guild.id, endpoint=str(ctx.guild.region)
+        )
+        should_connect = ctx.command.name in ("play",)
 
         if not ctx.author.voice or not ctx.author.voice.channel:
-            raise commands.CommandInvokeError('보이스채널에 연결되어있지 않습니다! 보이스채널에 들어가신후 저를 불러주세요!')
+            raise commands.CommandInvokeError(
+                "보이스채널에 연결되어있지 않습니다! 보이스채널에 들어가신후 저를 불러주세요!"
+            )
 
         if not player.is_connected:
             if not should_connect:
-                raise commands.CommandInvokeError('보이스채널에 연결되어있지 않습니다! 보이스채널에 들어가신후 저를 불러주세요!')
+                raise commands.CommandInvokeError(
+                    "보이스채널에 연결되어있지 않습니다! 보이스채널에 들어가신후 저를 불러주세요!"
+                )
 
             permissions = ctx.author.voice.channel.permissions_for(ctx.me)
 
             if not permissions.connect or not permissions.speak:
-                raise commands.CommandInvokeError(f'{ctx.bot.mention} 에게 보이스 연결, 말하기 권한이 있어야해요!')
+                raise commands.CommandInvokeError(
+                    f"{ctx.bot.mention} 에게 보이스 연결, 말하기 권한이 있어야해요!"
+                )
 
-            player.store('channel', ctx.channel.id)
+            player.store("channel", ctx.channel.id)
             await ctx.guild.change_voice_state(channel=ctx.author.voice.channel)
         else:
             if int(player.channel_id) != ctx.author.voice.channel.id:
-                raise commands.CommandInvokeError(f'{str(ctx.author)} 님은 제가 있는 보이스채널에 들어와주세요!')
+                raise commands.CommandInvokeError(
+                    f"{str(ctx.author)} 님은 제가 있는 보이스채널에 들어와주세요!"
+                )
 
     async def cog_before_invoke(self, ctx):
         guild_check = ctx.guild is not None
@@ -62,49 +77,52 @@ class Music(commands.Cog):
         ws = self.bot._connection._get_websocket(guild_id)
         await ws.voice_state(str(guild_id), channel_id)
 
-    @commands.command(aliases=['p'])
+    @commands.command(aliases=["p"])
     async def play(self, ctx: Context, *, query: str):
         player = self.bot.lavalink.player_manager.get(ctx.guild.id)
-        query = query.strip('<>')
+        query = query.strip("<>")
         if not url_rx.match(query):
-            query = f'ytsearch:{query}'
+            query = f"ytsearch:{query}"
 
         results = await player.node.get_tracks(query)
 
-        if not results or not results['tracks']:
-            return await ctx.reply('곡을 찾을수 없습니다.')
+        if not results or not results["tracks"]:
+            return await ctx.reply("곡을 찾을수 없습니다.")
 
         embed = discord.Embed(color=discord.Color.blurple())
 
-        if results['loadType'] == 'PLAYLIST_LOADED':
-            tracks = results['tracks']
+        if results["loadType"] == "PLAYLIST_LOADED":
+            tracks = results["tracks"]
 
             for track in tracks:
                 player.add(requester=ctx.author.id, track=track)
 
-            embed.title = '플레이리스트 적용 완료!'
+            embed.title = "플레이리스트 적용 완료!"
             embed.add_field(
-                name='타이틀',
-                value=f'{results["playlistInfo"]["name"]} - {len(tracks)} tracks'
+                name="타이틀",
+                value=f'{results["playlistInfo"]["name"]} - {len(tracks)} tracks',
             )
-            embed.set_image(url=f'https://i.ytimg.com/vi/{tracks["info"]["identifier"]}/hqdefault.jpg')
+            embed.set_image(
+                url=f'https://i.ytimg.com/vi/{tracks["info"]["identifier"]}/hqdefault.jpg'
+            )
             embed.add_field(
-                name='시간',
-                value=lavalink.utils.format_time(tracks['info']['length']),
-                inline=False
+                name="시간",
+                value=lavalink.utils.format_time(tracks["info"]["length"]),
+                inline=False,
             )
         else:
-            track = results['tracks'][0]
-            embed.title = '플레이리스트 추가 완료!'
+            track = results["tracks"][0]
+            embed.title = "플레이리스트 추가 완료!"
             embed.add_field(
-                name='타이틀',
-                value=f'[{track["info"]["title"]}]({track["info"]["uri"]})'
+                name="타이틀", value=f'[{track["info"]["title"]}]({track["info"]["uri"]})'
             )
-            embed.set_image(url=f'https://i.ytimg.com/vi/{track["info"]["identifier"]}/hqdefault.jpg')
+            embed.set_image(
+                url=f'https://i.ytimg.com/vi/{track["info"]["identifier"]}/hqdefault.jpg'
+            )
             embed.add_field(
-                name='시간',
-                value=lavalink.utils.format_time(track['info']['length']),
-                inline=False
+                name="시간",
+                value=lavalink.utils.format_time(track["info"]["length"]),
+                inline=False,
             )
 
             track = lavalink.models.AudioTrack(track, ctx.author.id)
@@ -115,34 +133,40 @@ class Music(commands.Cog):
         if not player.is_playing:
             await player.play()
 
-    @commands.command(aliases=['dc', 'stop'])
+    @commands.command(aliases=["dc", "stop"])
     async def disconnect(self, ctx: Context):
         player = self.bot.lavalink.player_manager.get(ctx.guild.id)
 
         if not player.is_connected:
-            return await ctx.reply('재생하고 있지 않습니다.')
+            return await ctx.reply("재생하고 있지 않습니다.")
 
-        if not ctx.author.voice or (player.is_connected and ctx.author.voice.channel.id != int(player.channel_id)):
-            return await ctx.reply('제 보이스채널에 들어와있지 않습니다.')
+        if not ctx.author.voice or (
+            player.is_connected
+            and ctx.author.voice.channel.id != int(player.channel_id)
+        ):
+            return await ctx.reply("제 보이스채널에 들어와있지 않습니다.")
 
         player.queue.clear()
         await player.stop()
         await ctx.guild.change_voice_state(channel=None)
-        await ctx.reply('정상적으로 연결해제 되었습니다.')
+        await ctx.reply("정상적으로 연결해제 되었습니다.")
 
-    @commands.command(aliases=['skip', '스킵'])
+    @commands.command(name="skip", aliases=["스킵"])
     async def skip(self, ctx: Context):
         player = self.bot.lavalink.player_manager.get(ctx.guild.id)
 
         if not player.is_connected:
-            return await ctx.reply('재생하고 있지 않습니다.')
+            return await ctx.reply("재생하고 있지 않습니다.")
 
-        if not ctx.author.voice or (player.is_connected and ctx.author.voice.channel.id != int(player.channel_id)):
-            return await ctx.reply('제 보이스채널에 들어와있지 않습니다.')
+        if not ctx.author.voice or (
+            player.is_connected
+            and ctx.author.voice.channel.id != int(player.channel_id)
+        ):
+            return await ctx.reply("제 보이스채널에 들어와있지 않습니다.")
 
         await player.skip()
-        await ctx.guild.change_voice_state(channel=None)
-        await ctx.message.add_reaction('✅')
+        await ctx.message.add_reaction("✅")
+
 
 def setup(bot: AutoShardedBot):
     bot.add_cog(Music(bot))
