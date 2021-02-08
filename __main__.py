7dac8147ce05cc5bd43f5d507ced6146a07385ee
unknown <storm@uniquecode.team>
2021-02-08 15:54:58 +0900

    feat: Default Music

--- __main__.py
+++ __main__.py
@@ -1,12 +1,16 @@
 from discord.ext import commands
 from dotenv import load_dotenv
-load_dotenv(verbose=True)
 import os
+from utils.autocogs import AutoCogs
+
+load_dotenv(verbose=True)
 TOKEN = os.getenv('TOKEN')
+
 
 class Bot(commands.AutoShardedBot):
     def __init__(self):
         super().__init__(command_prefix='../')
+        AutoCogs(self)
 
     async def on_ready(self):
         print(f'{str(self.user)} on Ready.')
