2903b82739b06d8a93f29364a7fe86d93d67d572
Tim232 <junseongi284@gmail.com>
2021-02-09 00:13:35 +0900

    Update autocogs.py

--- utils/autocogs.py
+++ utils/autocogs.py
@@ -2,7 +2,7 @@ import os
 
 
 def AutoCogs(self):
-    cogs = [i.replace(".py", "") for i in os.listdir("./cogs") if i.endswith(".py")]
+    cogs = [i[:-3] for i in os.listdir("./cogs") if i.endswith(".py")]
     for i in cogs:
         self.load_extension(f"cogs.{i}")
         print(f"{i}.py on Ready.")
